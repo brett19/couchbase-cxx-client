768fbcada78ef4651c8a31fc239d9b59770fc3d2
github-action-benchmark <github@users.noreply.github.com>
2021-11-03 22:30:24 +0000

    add Neo (catch2) benchmark result for d87b5ed2dba230ea74c8120143a2fb781e834314

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635978506561,
+  "lastUpdate": 1635978624187,
   "repoUrl": "https://github.com/couchbaselabs/couchbase-cxx-client",
   "entries": {
     "Mad Hatter": [
@@ -25,6 +25,35 @@ window.BENCHMARK_DATA = {
             "name": "get",
             "value": 344.781,
             "range": "± 52.0124",
+            "unit": "us",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      }
+    ],
+    "Neo": [
+      {
+        "commit": {
+          "author": {
+            "name": "couchbaselabs",
+            "username": "couchbaselabs"
+          },
+          "committer": {
+            "name": "couchbaselabs",
+            "username": "couchbaselabs"
+          },
+          "id": "d87b5ed2dba230ea74c8120143a2fb781e834314",
+          "message": "add benchmarking infrastructure",
+          "timestamp": "2021-11-03T20:04:35Z",
+          "url": "https://github.com/couchbaselabs/couchbase-cxx-client/pull/32/commits/d87b5ed2dba230ea74c8120143a2fb781e834314"
+        },
+        "date": 1635978622926,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "get",
+            "value": 392.577,
+            "range": "± 66.5826",
             "unit": "us",
             "extra": "100 samples\n1 iterations"
           }
