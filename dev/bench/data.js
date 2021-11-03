fc55e2bf233a927df0ff67724fdca87d67205263
github-action-benchmark <github@users.noreply.github.com>
2021-11-03 22:28:26 +0000

    add Mad Hatter (catch2) benchmark result for d87b5ed2dba230ea74c8120143a2fb781e834314

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1635978506561,
+  "repoUrl": "https://github.com/couchbaselabs/couchbase-cxx-client",
+  "entries": {
+    "Mad Hatter": [
+      {
+        "commit": {
+          "author": {
+            "name": "couchbaselabs",
+            "username": "couchbaselabs"
+          },
+          "committer": {
+            "name": "couchbaselabs",
+            "username": "couchbaselabs"
+          },
+          "id": "d87b5ed2dba230ea74c8120143a2fb781e834314",
+          "message": "add benchmarking infrastructure",
+          "timestamp": "2021-11-03T20:04:35Z",
+          "url": "https://github.com/couchbaselabs/couchbase-cxx-client/pull/32/commits/d87b5ed2dba230ea74c8120143a2fb781e834314"
+        },
+        "date": 1635978505812,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "get",
+            "value": 344.781,
+            "range": "± 52.0124",
+            "unit": "us",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
