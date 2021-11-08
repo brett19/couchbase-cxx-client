090d8834f30fefe658bac32c65e8ebe90e5bd893
github-action-benchmark <github@users.noreply.github.com>
2021-11-08 17:31:53 +0000

    add Neo (catch2) benchmark result for 7d3097adf06225768fbe57e1a5a87752553bbe95

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635978624187,
+  "lastUpdate": 1636392713480,
   "repoUrl": "https://github.com/couchbaselabs/couchbase-cxx-client",
   "entries": {
     "Mad Hatter": [
@@ -54,6 +54,37 @@ window.BENCHMARK_DATA = {
             "name": "get",
             "value": 392.577,
             "range": "± 66.5826",
+            "unit": "us",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sergey.avseyev@gmail.com",
+            "name": "Sergey Avseyev",
+            "username": "avsej"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7d3097adf06225768fbe57e1a5a87752553bbe95",
+          "message": "add benchmarking infrastructure (#32)",
+          "timestamp": "2021-11-08T17:08:04Z",
+          "tree_id": "78072287b54e8c1737b759031d0d60ed9fe585ed",
+          "url": "https://github.com/couchbaselabs/couchbase-cxx-client/commit/7d3097adf06225768fbe57e1a5a87752553bbe95"
+        },
+        "date": 1636392712232,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "get",
+            "value": 377.781,
+            "range": "± 48.0308",
             "unit": "us",
             "extra": "100 samples\n1 iterations"
           }
