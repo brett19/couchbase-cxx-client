5031ec79a1c3ad1a416f37e797862dc86f3911c3
github-action-benchmark <github@users.noreply.github.com>
2021-11-24 20:43:13 +0000

    add Mad Hatter (catch2) benchmark result for 9c436d9837d6591b074091a2d7086df735bd4e70

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637786574679,
+  "lastUpdate": 1637786593122,
   "repoUrl": "https://github.com/couchbaselabs/couchbase-cxx-client",
   "entries": {
     "Mad Hatter": [
@@ -738,6 +738,37 @@ window.BENCHMARK_DATA = {
             "name": "get",
             "value": 644.139,
             "range": "± 712.563",
+            "unit": "us",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jake.rawsthorne@couchbase.com",
+            "name": "Jake Rawsthorne",
+            "username": "jrawsthorne"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9c436d9837d6591b074091a2d7086df735bd4e70",
+          "message": " SDKQE-2499: Add query index management tests (#61)\n\n* Fix error code for index not found\r\n\r\n* SDKQE-2499: Add query index management tests\r\n\r\n* Use raw strings in queries instead of escaping\r\n\r\n* fix comment on the error code\r\n\r\nCo-authored-by: Sergey Avseyev <sergey.avseyev@gmail.com>",
+          "timestamp": "2021-11-24T20:24:57Z",
+          "tree_id": "42fa53015606775ee9f3ef621a8fb351d40b8515",
+          "url": "https://github.com/couchbaselabs/couchbase-cxx-client/commit/9c436d9837d6591b074091a2d7086df735bd4e70"
+        },
+        "date": 1637786591912,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "get",
+            "value": 338.791,
+            "range": "± 32.6572",
             "unit": "us",
             "extra": "100 samples\n1 iterations"
           }
